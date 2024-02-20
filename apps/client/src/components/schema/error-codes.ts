4950432a20bc9cac4e0cf87ac07e85c766d41a3f
Jon Chin <jon@sharebyteaching.org>
2024-02-20 13:21:07 -0500

    morehotfix

---
+++ apps/client/src/components/schema/error-codes.ts
@@ -0,0 +1,4 @@
+export const functionsErrorCodes = {
+  invalidArgumentEmailDomain: 'error-functions-invalid-argument-email-domain',
+  invalidArgumentSchema: 'error-functions-invalid-argument-schema',
+};
