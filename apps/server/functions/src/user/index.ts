447cd88fbd2ffbb580826ae6d4f734fe480cab21
Jon Chin <jon@sharebyteaching.org>
2024-04-23 12:52:39 -0400

    initial support

--- apps/server/functions/src/user/index.ts
+++ apps/server/functions/src/user/index.ts
@@ -1,9 +1,11 @@
+import {close} from './close';
 import {community} from './community';
 import {create} from './create';
 import {logPostView} from './logPostView';
 import {messagingToken} from './messagingToken';
 
 export const user = {
+  close,
   community,
   create,
   log: {
